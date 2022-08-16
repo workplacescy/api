c853c9284f02a80b74c4a11d19df6d504f473940
Vladimir Martsul <vladimir.martsul@gmail.com>
2022-08-16 16:20:37 +0300

    Simplify response
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- app/Http/Resources/PlaceResource.php
+++ app/Http/Resources/PlaceResource.php
@@ -8,26 +8,25 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use OpenApi\Attributes as OA;
 
-#[OA\Schema(required: ['id', 'type', 'properties'], properties: [
+#[OA\Schema(required: ['id', 'name', 'description', 'type', 'city', 'placeId', 'address', 'position', 'url', 'sockets', 'size', 'location', 'busyness', 'view', 'rank'], properties: [
     new OA\Property(property: 'id', type: 'integer'),
-    new OA\Property(property: 'type', type: 'string', enum: ['Feature']),
-    new OA\Property(property: 'properties', required: ['name', 'description', 'type', 'placeId', 'city', 'sockets', 'size', 'location', 'busyness', 'view', 'rank'], properties: [
-        new OA\Property(property: 'name', type: 'string'),
-        new OA\Property(property: 'description', type: 'string'),
-        new OA\Property(property: 'type', type: 'string'),
-        new OA\Property(property: 'placeId', type: 'string'),
-        new OA\Property(property: 'city', type: 'string'),
-        new OA\Property(property: 'sockets', type: 'string'),
-        new OA\Property(property: 'size', type: 'string'),
-        new OA\Property(property: 'location', type: 'string'),
-        new OA\Property(property: 'busyness', type: 'string'),
-        new OA\Property(property: 'view', type: 'string'),
-        new OA\Property(property: 'rank', type: 'integer'),
+    new OA\Property(property: 'name', type: 'string'),
+    new OA\Property(property: 'description', type: 'string'),
+    new OA\Property(property: 'type', type: 'string'),
+    new OA\Property(property: 'city', type: 'string'),
+    new OA\Property(property: 'placeId', type: 'string'),
+    new OA\Property(property: 'address', type: 'string'),
+    new OA\Property(property: 'position', required: ['lat', 'lng'], properties: [
+        new OA\Property(property: 'lat', type: 'number', format: 'float'),
+        new OA\Property(property: 'lng', type: 'number', format: 'float'),
     ], type: 'object'),
-    new OA\Property(property: 'geometry', required: ['type', 'coordinates'], properties: [
-        new OA\Property(property: 'type', type: 'string', enum: ['Point']),
-        new OA\Property(property: 'coordinates', type: 'array', items: new OA\Items(type: 'number', format: 'float')),
-    ], type: 'object'),
+    new OA\Property(property: 'url', type: 'string'),
+    new OA\Property(property: 'sockets', type: 'string'),
+    new OA\Property(property: 'size', type: 'string'),
+    new OA\Property(property: 'location', type: 'string'),
+    new OA\Property(property: 'busyness', type: 'string'),
+    new OA\Property(property: 'view', type: 'string'),
+    new OA\Property(property: 'rank', type: 'integer'),
 ], type: 'object')]
 final class PlaceResource extends JsonResource
 {
@@ -40,27 +39,23 @@ final class PlaceResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'type' => 'Feature',
-            'properties' => [
-                'name' => $this->name,
-                'description' => $this->description,
-                'type' => $this->type,
-                'placeId' => $this->place_id,
-                'city' => $this->city,
-                'sockets' => $this->sockets,
-                'size' => $this->size,
-                'location' => $this->location,
-                'busyness' => $this->busyness,
-                'view' => $this->view,
-                'rank' => $this->rank,
+            'name' => $this->name,
+            'description' => $this->description,
+            'type' => $this->type,
+            'city' => $this->city,
+            'placeId' => $this->place_id,
+            'address' => $this->address,
+            'position' => [
+                'lat' => (float)$this->longitude,
+                'lng' => (float)$this->latitude,
             ],
-            'geometry' => [
-                'type' => 'Point',
-                'coordinates' => [
-                    (float)$this->longitude,
-                    (float)$this->latitude,
-                ],
-            ],
+            'url' => $this->url,
+            'sockets' => $this->sockets,
+            'size' => $this->size,
+            'location' => $this->location,
+            'busyness' => $this->busyness,
+            'view' => $this->view,
+            'rank' => $this->rank,
         ];
     }
 }
