a0cfbab1a5387437ff3c6abffbd77cb940e27341
Vladimir Martsul <vladimir.martsul@gmail.com>
2022-12-02 18:08:13 +0200

    Show place not found error in JSON
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,14 @@ use App\Http\Controllers\NearestController;
 use App\Http\Controllers\RandomController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\ShowController;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
-Route::middleware('api')->group(static function (): void {
-    Route::get('/', IndexController::class)->name('index');
-    Route::get('/random', RandomController::class)->name('random');
-    Route::get('/search', SearchController::class)->name('search');
-    Route::get('/nearest', NearestController::class)->name('nearest');
-    Route::get('/{place}', ShowController::class)->name('place');
-});
+Route::get('/', IndexController::class);
+Route::get('/random', RandomController::class);
+Route::get('/search', SearchController::class);
+Route::get('/nearest', NearestController::class);
+Route::get('/{place}', ShowController::class)->missing(static fn(): Response => Route::respondWithRoute('fallback'));
+
+Route::fallback(static fn(): JsonResponse => response()->json(['error' => 'Not Found'], Response::HTTP_NOT_FOUND))->name('fallback');
